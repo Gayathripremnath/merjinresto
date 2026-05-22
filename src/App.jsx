88d0df5fea6cc44a928c78d56bb8594207ffb333
Gayathripremnath <gayathripremnath7@gmail.com>
2026-05-22 14:14:55 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const About = lazy(() => import('./components/About'));
 const Booking = lazy(() => import('./components/Booking'));
 const Invoice = lazy(() => import('./components/Invoice'));
 const Gallery = lazy(() => import('./components/Gallery'));
-const Contact = lazy(() => import('./components/ContactUs'));
+const ContactUs = lazy(() => import('./components/ContactUs'));
 
 function App() {
   return (
