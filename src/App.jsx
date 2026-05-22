d520dca8aec6a95be6837ecfab8d78c12926b840
Gayathripremnath <gayathripremnath7@gmail.com>
2026-05-22 12:57:42 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ const About = lazy(() => import('./components/About'));
 const Booking = lazy(() => import('./components/Booking'));
 const Invoice = lazy(() => import('./components/Invoice'));
 const Gallery = lazy(() => import('./components/Gallery'));
-// const ContactUs = lazy(() => import('./components/ContactUs'));
 
 function App() {
   return (
@@ -29,7 +28,6 @@ function App() {
           <Route path="/booking" element={<Booking />} />
           <Route path="/invoice" element={<Invoice />} />
           <Route path="/gallery" element={<Gallery />} />
-          {/* <Route path="/contact" element={<ContactUs />} /> */}
         </Routes>
       </Suspense>
       <Footer />
